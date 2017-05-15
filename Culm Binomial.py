2f635565671a7ed1945f2ec62118704b857eb51b
mwcotton <matthew.cotton16@imperial.ac.uk>
2017-05-15 16:32:29 +0100

    Add files via upload

---
+++ Culm Binomial.py
@@ -0,0 +1,26 @@
+import scipy.misc as scm
+import numpy.random as npr
+import numpy as np
+import matplotlib.pyplot as plt
+
+def culmBinom(p, n, rand):
+    s = 0
+    prob = 1
+    while (1):
+        prob -= (p**(n-s))*((1-p)**s)*(scm.comb(n, s))
+        if rand > prob:
+            return(s)
+            break
+        else:
+            s += 1
+
+g = 100
+p = 0.5
+
+values = np.zeros(g+1)
+for x in range(100000):
+    values[culmBinom(p, g, npr.uniform())] += 1
+
+plt.plot(range(g+1), values)
+
+plt.show()
