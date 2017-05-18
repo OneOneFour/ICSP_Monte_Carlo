090f37040c22fa776c6a5b3f4f6bf99f50267203
mwcotton <matthew.cotton16@imperial.ac.uk>
2017-05-18 20:23:54 +0100

    CSV save function and fixed culmBinom

---
+++ ProjectFunctions.py
@@ -0,0 +1,23 @@
+import scipy.misc as spm
+
+def culmBinom(p, n):
+    s = 0
+    prob = 1
+    rand = npr.uniform() #generates a random number to compare the probability to
+    while (1):
+        prob -= (p**s)*((1-p)**(n-s))*(scm.comb(n, s)) #adjust the parameters of prob to allow for the new number of events
+        if rand > prob: #see if the randomly generated number lies in the region corresponding to s events
+            return(s) #if the randomly generated number lies within the section for this area of prob, retun the number of successes
+            break
+        else:
+            s += 1
+
+    fd = open(fdir,'a')
+    if text:
+        print("Found the file....")
+    fd.write("{}, {}, {}, {}\n".format(a, b, c, d))
+    if text:
+        print("Saving....")
+    fd.close()
+    if text:
+        print("Saved!")
