9858bc00ee038124281e1f9e36de4e02eab9a308
Robert King <robertkng104@gmail.com>
2017-05-18 23:33:56 +0100

    Monte-Carlo now works as predators can now eat new born prey. Model is roughly similar to lotka-volterra model

--- main.py
+++ main.py
@@ -8,14 +8,14 @@ def lotkarra(s,t,alp,bet,gam,delt):
     dsdt = [alp*x - bet * x*y,delt * x * y - gam * y]
     return dsdt
 
-alp,bet,gam,delt = 0.66,1.33,1,1
+alp,bet,gam,delt = 1,0.2,0.6,1
 t = np.linspace(0,10,1000)
 plt.gca().set_xlabel("Time")
 plt.gca().set_ylabel("Predator count")
-for i in np.arange(0.5,1.5,0.2):
-    s0 = [i,i]
-    ans = scint.odeint(lotkarra, s0, t, args=(alp, bet, gam, delt))
-    plt.plot(t,ans[:,1],linewidth=1,label=i)
-plt.legend(bbox_to_anchor=(1, 1), loc=2)
+s0 = [1,1]
+ans = scint.odeint(lotkarra, s0, t, args=(alp, bet, gam, delt))
+plt.plot(t,ans[:,0], linewidth=1,label="prey")
+plt.plot(t,ans[:,1],linewidth=1,label = "pred")
+plt.legend()
 print(time.time() - startTime)
 plt.show()
